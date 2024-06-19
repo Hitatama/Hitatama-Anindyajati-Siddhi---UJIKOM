b11a98d419c31108298f0a8522a0d1f31cd23684
Hitatama <hitatama56@gmail.com>
2024-06-19 12:07:34 +0700

    add method scene load

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    private bool isPaused;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Continou();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Continou()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+}
